b6c61c1049f6b89692b3ee41d57ba59f25f1845c
Marc Hildenbrand <mwhildenbrand+github@gmail.com>
2020-03-13 07:23:19 +0000

    Test code deploy

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'My Demo Page - Local Checkin' });
+  res.render('index', { title: 'My Demo Page - CodeDeploy Checkin' });
 });
 
 module.exports = router;
